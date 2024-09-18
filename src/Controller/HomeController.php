46ecb4433cdc9b45e1e390dd9615132769cb5bf0
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2024-09-18 11:39:10 +0100

    first pages:home and contact

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+
+}
